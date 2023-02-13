512588997931f712b2438e8b4a64fb7819027a57
Jacob Lamont <cob@jacoblamont.com>
2023-02-13 18:47:03 -0500

    removes sizing chart image element if no image exists

--- src/routes/p/[product=slug]/[[printfulVariantId=int]]/MeasurementDetails.svelte
+++ src/routes/p/[product=slug]/[[printfulVariantId=int]]/MeasurementDetails.svelte
@@ -7,7 +7,9 @@
 
 <h3 class="text-h1">{heading}</h3>
 
-<img class="py-6 h-[150px]" src={details.image_url} alt="" />
+{#if details.image_url}
+  <img class="py-6 h-[150px]" src={details.image_url} alt="product sizing graphic" />
+{/if}
 
 <div class="w-full overflow-x-auto">
   <SizeChart measurements={details.measurements} unit={details.unit} />
