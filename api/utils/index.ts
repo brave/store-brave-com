62f7c5ccb525c996bf2daf498d0732521387cfb6
Jacob Lamont <cob@jacoblamont.com>
2023-03-09 18:34:22 -0500

    adds admin page to decrypt recipient address

--- api/utils/index.ts
+++ api/utils/index.ts
@@ -2,6 +2,10 @@ import https from 'https';
 import crypto from 'crypto';
 import { Upload } from '@aws-sdk/lib-storage';
 import { ListObjectsCommand, S3Client } from '@aws-sdk/client-s3';
+import tweetnacl from 'tweetnacl';
+const { secretbox, randomBytes } = tweetnacl;
+import { encode as encodeUTF8, decode as decodeUTF8 } from '@stablelib/utf8';
+import { encode as encodeBase64, decode as decodeBase64 } from '@stablelib/base64';
 import * as Sentry from '@sentry/node';
 import dotenv from 'dotenv';
 
@@ -296,4 +300,29 @@ export const purgeOldShippingDataKeys = async (context: KeystoneContext) => {
     console.log(e);
     Sentry.captureMessage(`Could not purge old shipping data keys.`, 'error');
   }
+};
+
+/**
+ * Encrypt/Decrypt utils
+ * HT: https://github.com/dchest/tweetnacl-js/wiki/Examples#secretbox
+ */
+export const generateKey = () => encodeBase64(randomBytes(secretbox.keyLength));
+
+export const decrypt = (messageWithNonce: string, key: string) => {
+  const keyUint8Array = decodeBase64(key);
+  const messageWithNonceAsUint8Array = decodeBase64(messageWithNonce);
+  const nonce = messageWithNonceAsUint8Array.slice(0, secretbox.nonceLength);
+  const message = messageWithNonceAsUint8Array.slice(
+    secretbox.nonceLength,
+    messageWithNonce.length
+  );
+
+  const decrypted = secretbox.open(message, nonce, keyUint8Array);
+
+  if (!decrypted) {
+    throw new Error('Could not decrypt message');
+  }
+
+  const base64DecryptedMessage = decodeUTF8(decrypted);
+  return JSON.parse(base64DecryptedMessage);
 };
