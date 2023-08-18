993c43f9453d929075a0e2df739faf2756d1cff1
Jacob Lamont <cob@jacoblamont.com>
2023-08-18 13:32:17 -0400

    adds screens/opacity config to local tailwind config
    These configuration were removed from Leo's Tailwind preset.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,38 @@ module.exports = {
   darkMode: ['class', `[data-theme="dark"]`],
   content: ['./src/**/*.{html,js,svelte,ts}'],
   presets: [require('@brave/leo/build/tailwind')],
-  theme: {},
+  theme: {
+    screens: {
+      xxs: '360px',
+      xs: '440px',
+      sm: '560px',
+      'md-sm': '641px',
+      md: '821px',
+      lg: '1141px',
+      xl: '1440px'
+    },
+    opacity: {
+      0: '0',
+      10: '.1',
+      15: '.15',
+      20: '.2',
+      25: '.25',
+      30: '.3',
+      35: '.35',
+      40: '.4',
+      45: '.45',
+      50: '.5',
+      55: '.55',
+      60: '.6',
+      65: '.65',
+      70: '.7',
+      75: '.75',
+      80: '.8',
+      85: '.85',
+      90: '.9',
+      95: '.95',
+      100: '1'
+    }
+  },
   plugins: []
 };
