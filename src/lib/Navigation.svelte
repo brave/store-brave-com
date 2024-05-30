aa345bc818d09096420e2236946d94cc0aa44f9c
Jacob Lamont <cob@jacoblamont.com>
2024-05-30 18:45:13 -0400

    chore: format repo

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -1,23 +1,34 @@
 <script lang="ts">
   import { getContext } from 'svelte';
-  import { page as currentPage } from "$app/stores";
+  import { page as currentPage } from '$app/stores';
   import { contextKey } from '$lib/cartStore';
   import CartButton from '$lib/CartButton.svelte';
-  import Icon from "@brave/leo/src/components/icon/icon.svelte";
+  import Icon from '@brave/leo/src/components/icon/icon.svelte';
 
   const { cartStore }: any = getContext(contextKey);
 
-  $: totalCartItems = $cartStore.reduce((sum: number, item: App.CartItem) => sum += item.quantity, 0);
+  $: totalCartItems = $cartStore.reduce(
+    (sum: number, item: App.CartItem) => (sum += item.quantity),
+    0
+  );
 
   const pages = [
     { name: 'Home', permalink: '/' },
-    { name: 'Products', permalink: '/categories/all/', isActive: () => $currentPage.url.pathname.startsWith("/categories/") },
-    { name: 'Privacy', permalink: 'https://brave.com/privacy/website/#brave-merch-store', newTab: true }
+    {
+      name: 'Products',
+      permalink: '/categories/all/',
+      isActive: () => $currentPage.url.pathname.startsWith('/categories/')
+    },
+    {
+      name: 'Privacy',
+      permalink: 'https://brave.com/privacy/website/#brave-merch-store',
+      newTab: true
+    }
   ];
 
   let mobileMenuOpen = false;
-  const openMobileMenu = () => mobileMenuOpen = true;
-  const closeMobileMenu = () => mobileMenuOpen = false;
+  const openMobileMenu = () => (mobileMenuOpen = true);
+  const closeMobileMenu = () => (mobileMenuOpen = false);
 </script>
 
 <div
@@ -38,16 +49,26 @@
       src="/images/brave-logo-dark.svg"
       alt="Brave Merch Store"
     />
+    <!-- prettier-ignore -->
     <span class="logo-tag text-heading-h2 whitespace-nowrap text-text-primary opacity-90 ml-m font-normal">| merch</span>
   </a>
 
   <nav class="flex gap-x-3xl">
     <ul class="hidden md:flex items-center gap-x-3xl">
       {#each pages as page}
-        <li class="nav-item after:content-[''] h-full flex items-center" class:active={page.isActive?.() || page.permalink === $currentPage.url.pathname}><a class="nav-link" href={page.permalink} target={page.newTab ? "_blank" : null}>{page.name}</a></li>
+        <li
+          class="nav-item after:content-[''] h-full flex items-center"
+          class:active={page.isActive?.() || page.permalink === $currentPage.url.pathname}
+        >
+          <!-- prettier-ignore -->
+          <a class="nav-link" href={page.permalink} target={page.newTab ? '_blank' : null}>{page.name}</a>
+        </li>
       {/each}
     </ul>
-    <span class="nav-item after:content-none md:after:content-[''] flex items-center" class:active={"/cart/" === $currentPage.url.pathname}>
+    <span
+      class="nav-item after:content-none md:after:content-[''] flex items-center"
+      class:active={'/cart/' === $currentPage.url.pathname}
+    >
       <CartButton ref="cart-button" itemCount={totalCartItems} />
     </span>
   </nav>
@@ -55,7 +76,9 @@
 
 <!-- BEGIN mobile nav -->
 <div class="mobile-menu fixed top-0 left-0 min-h-screen w-screen z-10" class:open={mobileMenuOpen}>
-  <nav class="mobile-menu__nav w-4/5 bg-container-background min-h-screen z-10 pl-2xl pr-xl py-[20px]">
+  <nav
+    class="mobile-menu__nav w-4/5 bg-container-background min-h-screen z-10 pl-2xl pr-xl py-[20px]"
+  >
     <div class="flex justify-between pb-5xl">
       <a class="flex items-center" href="/" on:click={closeMobileMenu}>
         <img class="logo inline dark:hidden" src="/images/brave-logo.svg" alt="Brave Merch Store" />
@@ -64,6 +87,7 @@
           src="/images/brave-logo-dark.svg"
           alt="Brave Merch Store"
         />
+        <!-- prettier-ignore -->
         <span class="logo-tag text-heading-h2 whitespace-nowrap text-primary opacity-90 ml-m font-normal">| merch</span>
       </a>
 
@@ -74,18 +98,26 @@
 
     <ul class="flex flex-col gap-y-3xl">
       {#each pages as page}
-        <li class="nav-item h-full flex items-center" class:active={page.permalink === $currentPage.url.pathname}><a on:click={closeMobileMenu} class="nav-link" href={page.permalink}>{page.name}</a></li>
+        <li
+          class="nav-item h-full flex items-center"
+          class:active={page.permalink === $currentPage.url.pathname}
+        >
+          <a on:click={closeMobileMenu} class="nav-link" href={page.permalink}>{page.name}</a>
+        </li>
       {/each}
     </ul>
   </nav>
 </div>
+
 <!-- END mobile nav -->
 
 <style lang="scss">
-
   .mobile-menu {
     visibility: hidden;
-    transition: visibility 250ms ease-in, backdrop-filter 250ms ease-in, background-color 250ms ease-in;
+    transition:
+      visibility 250ms ease-in,
+      backdrop-filter 250ms ease-in,
+      background-color 250ms ease-in;
 
     &__nav {
       transform: translateX(-100%);
@@ -120,8 +152,8 @@
         background: theme('colors.text.interactive');
 
         position: absolute;
-          top: 50%;
-          left: -1.5rem;
+        top: 50%;
+        left: -1.5rem;
         transform: translateY(-50%);
       }
     }
