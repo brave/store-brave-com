17d4af77c154432572401a1bee4f161b105d869a
Jacob Lamont <cob@jacoblamont.com>
2024-05-30 00:12:16 -0400

    feat: adds category nav buttons to category pages
    Resolves https://github.com/brave/store-brave-com/issues/12

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,12 +2,10 @@ import { sdk } from '$lib/graphql/sdk';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-  const { categories } = await sdk.Categories();
   const { products } = await sdk.FeaturedProducts();
 
   return {
     title: "Home",
-    categories,
     featuredProducts: products
   };
 }
