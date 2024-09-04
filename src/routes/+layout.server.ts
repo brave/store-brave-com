fee4b10e322ef61f5873295c118d1470eba91feb
Jacob Lamont <cob@jacoblamont.com>
2024-09-04 14:18:35 -0400

    fix: fixes build error when prerendering

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,9 @@
 import { sdk } from '$lib/graphql/sdk';
 
 /** @type {import('./$types').LayoutServerLoad} */
-export async function load() {
+export async function load({ route }) {
+  if (route.id === '/[slug=slug]') return;
+
   const { categories } = await sdk.Categories();
 
   return {
