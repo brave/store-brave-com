3c20f65e4c2d779576f25a9438a874b18dad8b94
Jacob Lamont <cob@jacoblamont.com>
2026-03-23 15:05:26 -0400

    chore(deps): bumps versions for audit fixes

--- src/lib/stripe-api.ts
+++ src/lib/stripe-api.ts
@@ -3,6 +3,7 @@ import { env } from '$env/dynamic/private';
 import { building } from '$app/environment';
 
 // @ts-ignore
-export const stripe = new Stripe(!building && env.STRIPE_KEY, {
+export const stripe = new Stripe(!building ? env.STRIPE_KEY : 'no_key_during_build_stage', {
+  // @ts-ignore stripe-version-2022-11-15
   apiVersion: '2022-11-15'
 });
