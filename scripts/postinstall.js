f7faa0fbefa3ec28d02ed7182ba401b9b7183f31
Jacob Lamont <cob@jacoblamont.com>
2026-06-05 15:26:59 -0400

    fixup! fix(ci): fix build for fe

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,5 +1,5 @@
 import { execSync } from 'child_process'
 
 if (!process.env.SKIP_POSTINSTALL) {
-  execSync('node scripts/copy-dependencies.js && pnpm gen', { stdio: 'inherit', shell: true })
+  execSync('node scripts/copy-dependencies.js && pnpm gen', { stdio: 'inherit' })
 }
