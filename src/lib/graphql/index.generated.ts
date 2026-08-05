45780ffe37cb9e457ca23cd930c9f940f6e2612e
Jacob Lamont <cob@jacoblamont.com>
2026-08-05 15:42:49 -0400

    chore: bumps dependencies

--- src/lib/graphql/index.generated.ts
+++ src/lib/graphql/index.generated.ts
@@ -2,7 +2,7 @@
 type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 /** Internal type. DO NOT USE DIRECTLY. */
 export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
-import type { GraphQLClient, RequestOptions } from 'graphql-request';
+import { type GraphQLClient, type RequestOptions } from 'graphql-request';
 import gql from 'graphql-tag';
 type GraphQLClientRequestHeaders = RequestOptions['requestHeaders'];
 export type FeaturedProductsQueryVariables = Exact<{ [key: string]: never; }>;
